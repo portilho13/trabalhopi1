6d97c003f06244cfe416bdbd6a37191da08f20ba
portilho13 <junior.portilho2005@gmail.com>
2023-11-30 20:04:38 +0000

    Change in main #2

--- main.c
+++ main.c
@@ -9,12 +9,9 @@ struct pessoa {
     char nome[TAMSTR];
     int codigo;
     long tel;
-    int caloriasMin;
-
 };
 
 typedef struct pessoa Atleta;
-
 
 struct prato {
     int codigo;
@@ -25,6 +22,15 @@ struct prato {
 };
 typedef struct prato Refeicao;
 
+struct plano {
+    int codigo;
+    int dia;
+    int mes;
+    int ano;
+    int calMin;
+    int calMax;
+};
+typedef struct plano Planos;
 
 void lerArquivo(char arquivo[5]);
 
@@ -33,62 +39,99 @@ int main() {
     return 0;
 }
 
-
-void lerArquivo(char arquivo[5]) { // Funcao ler arquivo
-    FILE *fOne, *fTwo;
+void lerArquivo(char arquivo[5]) {
+    FILE *fOne, *fTwo, *fThree;
     char ch;
-    int i = 0, codigo;
+    int i = 0, j, k, codigo, dia, mes, ano, calMin, calMax, qtdClientes, qtdPratos, qtdPlanos;
     long tel;
 
     Atleta Cliente[TAM];
     char nome[TAMSTR];
-    
-    if ((fOne = fopen(arquivo, "r")) == NULL) // Se ha erro ao aceder ao ficheiro return 1
-    {
-        fclose(fOne);
+
+    if ((fOne = fopen(arquivo, "r")) == NULL) {
+        printf("Error opening file %s\n", arquivo);
         return;
     }
-    
+
+    qtdClientes = 0;
     do {
         fscanf(fOne, "%d;%[^;];%ld\n", &codigo, nome, &tel);
         if (codigo >= 0 && codigo < TAM) {
-            Cliente[codigo].codigo = codigo;
-            strcpy(Cliente[codigo].nome, nome);
-            Cliente[codigo].tel = tel;
-            i++;
+            Cliente[i].codigo = codigo;
+            strcpy(Cliente[i].nome, nome);
+            Cliente[i].tel = tel;
         } else {
-            printf("Codigo Invalido: %d\n", codigo);
+            printf("Invalid Code: %d\n", codigo);
         }
+        i++;
     } while ((!feof(fOne) && (i < TAM)));
 
     fclose(fOne);
 
-    if ((fTwo = fopen("2.txt", "r")) == NULL) // Se ha erro ao aceder ao ficheiro return 1
-    {
-        fclose(fTwo);
+    if ((fTwo = fopen("2.txt", "r")) == NULL) {
+        printf("Error opening file 2.txt\n");
         return;
     }
 
     Refeicao Pratos[TAM];
-    int dia, mes, ano, calorias;
-    char refeicao[50];
-    char prato[50];
-
+    int calorias;
+    char refeicao[TAMSTR], prato[TAMSTR];
+    i = 0;
+    qtdPratos = 0;
     do {
         fscanf(fTwo, "%d;%d-%d-%d;%[^;];%[^;];%d cal", &codigo, &dia, &mes, &ano, refeicao, prato, &calorias);
         if (codigo >= 0 && codigo < TAM) {
-            Pratos[codigo].dia = dia;
-            Pratos[codigo].mes = mes;
-            Pratos[codigo].ano = ano;
-            Pratos[codigo].cal = calorias;
-
+            Pratos[i].dia = dia;
+            Pratos[i].mes = mes;
+            Pratos[i].ano = ano;
+            Pratos[i].cal = calorias;
         } else {
-            printf("Codigo Invalido: %d\n", codigo);
+            printf("Invalid Code: %d\n", codigo);
         }
-        printf("Codigo: %d; Dia: %d; Mes: %d; Ano: %d; Calorias: %d\n", codigo, Pratos[codigo].dia, Pratos[codigo].mes, Pratos[codigo].ano, Pratos[codigo].cal);
-        //printf("Total %d\n", Pratos[codigo].cal);
+        i++;
     } while (!feof(fTwo));
-    //printf("Total: %d\n", Pratos[1].cal);
-
     fclose(fTwo);
-}
+
+    if ((fThree = fopen("3.txt", "r")) == NULL) {
+        printf("Error opening file 3.txt\n");
+        return;
+    }
+
+    Planos Plano[TAM];
+    i = 0;
+    qtdPlanos = 0;
+    do {
+        // Initialize variables before the loop
+        codigo = dia = mes = ano = calMin = calMax = 0;
+        int teste = fscanf(fThree, "%d;%d-%d-%d;%[^;];%d Cal, %d Cal", &codigo, &dia, &mes, &ano, prato, &calMin, &calMax);
+        // Read data and check for successful reading
+        if (teste != 8) {
+            printf("%d", teste);
+            printf("Erro ao ler do ficheiro\n");
+            break;
+        }
+
+        if (codigo >= 0 && codigo < TAM) {
+            Plano[i].dia = dia;
+            Plano[i].mes = mes;
+            Plano[i].ano = ano;
+            Plano[i].calMin = calMin;
+            Plano[i].calMax = calMax;
+        } else {
+            printf("Invalid Code: %d\n", codigo);
+        }
+        i++;
+    } while (!feof(fThree));
+
+    fclose(fThree);
+
+    // int numCal;
+    // numCal = codigo = 0;
+    // for (int i = 0; i < qtdPratos; i++){
+    //    for (int j = 0; j < qtdPlanos; j++) {
+    //         if (Pratos[i].codigo == Plano[j].codigo) {
+    //             printf("%d", Plano[j].codigo);
+    //         }
+    //    }
+    // }
+ }
